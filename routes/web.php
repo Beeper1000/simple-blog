5746c902c18b6348cfe323c2742f1a7e346d0ae3
Leon <leon.meyer.zum.alten.borgloh@urs-os.de>
2023-02-09 14:46:11 +0100

    1. fertig - 09.02

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('home');
 });
 
 Route::get('/post', function () {
     return view('post');
 });
+/*
+            Route::get('/home', function () {
+                return view('home');
+            });
 
+            Route::get('/post', function () {
+                return view('post');
+            });
+           
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+ */
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
